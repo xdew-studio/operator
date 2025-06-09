e1947e30a097ef5f295e561ff9512fb986db764a
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-06-09 16:29:35 +0200

    feat: Initialize setup.py for xdew-operator package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xdew-operator',
+    version='2.0.0',
+    description='Kopf operator for managing and deploying projects on XDEW',
+    url='https://github.com/xdew-studio/operator',
+    packages=find_packages(),
+)
