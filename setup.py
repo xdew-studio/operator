95fa4295fd89e9f75bf9ec8b01e5dd2119c2addf
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-07-11 16:20:14 +0200

    [version] Bump to 2.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xdew-operator',
-    version='2.2.0',
+    version='2.2.1',
     description='Kopf operator for managing and deploying projects on XDEW',
     url='https://github.com/xdew-studio/operator',
     packages=find_packages(),
